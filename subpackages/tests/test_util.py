55b9fdb3851dd53f0e61ec7d004586adba0f4f9c
Kin Maynard <kmaynard@gm.slc.edu>
2021-10-30 17:54:36 -0400

    invert unit tests

--- subpackages/tests/test_util.py
+++ subpackages/tests/test_util.py
@@ -11,9 +11,15 @@ from subpackages.util.split import split
 from subpackages.util.invert import invert
 from subpackages.util.reverse import reverse
 
-array = np.arange(-1, 1)
-
 class TestUtil(unittest.TestCase):
 	
 	def test_invert(self):
-		self.assertEqual(invert(array), np.array([1, 0, 1]))
+		self.assertEqual(invert(np.arange(-1, 2)), np.negative(np.arange(-1, 2)))
+		self.assertEqual(invert(np.arange(4), np.negative(np.arange(4))))
+		self.assertEqual(invert(np.arange(-4, 4), np.negative(np.arange(-4, 4))))
+		self.assertEqual(invert(np.arange(-1, 3).reshape(2, 2)), np.negative(np.arange(-1, 3).reshape(2, 2)))
+		self.assertEqual(invert(np.arange(4).reshape(2, 2), np.negative(np.arange(4).reshape(2, 2))))
+		self.assertEqual(invert(np.arange(-4, 0).reshape(2, 2), np.negative(np.arange(-4, 0).reshape(2, 2))))
+
+	def test_reverse(self):
+		
--- subpackages/util/invert.py
+++ subpackages/util/invert.py
@@ -9,7 +9,7 @@ import numpy as np
 def invert(array):
 	'''
 	Inverts the phase (polarity) of an array of audio data
-	array: a numpy array of audio data
+	array: a numpy array of audio data (numbers), not empty
 	returns: a version of array with the polarity inverted
 	'''
 	# inverts polarity of audio data
