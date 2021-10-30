7670d7feffe2c6bc6961bb095c9fdda25d688f16
Kin Maynard <kmaynard@gm.slc.edu>
2021-10-30 19:45:16 -0400

    test run script

--- run_tests.py
+++ run_tests.py
@@ -1,1 +1,19 @@
 import unittest
+
+# import test modules
+
+from subpackages.tests import test_util
+
+# initialize test suite
+
+loader = unittest.TestLoader()
+suite = unittest.TestSuite()
+
+# add tests to suite
+
+suite.addTest(loader.loadTestsFromModule(test_util))
+
+# initialize test runner, run the suite
+
+runner = unittest.TextTestRunner()
+result = runner.run(suite)
