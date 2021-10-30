a11ac6a17688b97052b284cc4707e9b618f6d48a
Kin Maynard <kmaynard@gm.slc.edu>
2021-10-30 16:04:52 -0400

    remove import array import in testutil

--- subpackages/util/test_util.py
+++ subpackages/util/test_util.py
@@ -6,7 +6,6 @@ Unit Tests
 
 import unittest
 import numpy as np
-from subpackages.io.import_array import import_array
 from subpackages.util.trim import trim
 from subpackages.util.split import split
 from subpackages.util.invert import invert
