4a6a5caaef7a2cacda85fe15d6224f4704c5e808
Kin Maynard <kmaynard@gm.slc.edu>
2021-10-31 00:03:15 -0400

    dsp tests

--- subpackages/tests/test_dsp.py
+++ subpackages/tests/test_dsp.py
@@ -9,15 +9,12 @@ import numpy as np
 from subpackages.dsp.normalize import normalize
 from subpackages.dsp.midside import midside
 
-sign_arr = np.arange(-1, 2)
-p_arr = np.arange(6)
-n_arr = np.arange(-4, 0)
-dub_sign_arr = np.arange(-1, 3).reshape(2, 2)
-dub_p_arr = p_arr.reshape(3, 2)
-dub_n_arr = n_arr.reshape(2, 2)
-
 class TestDsp(unittest.TestCase):
 	
 	def test_midside(self):
+		self.assertEqual(midside(array, '1'), )
+		self.assertEqual(midside(array, '2'), )
+		self.assertEqual(midside(array, '2', False), )
 
 	def test_normalize(self):
+		self.assertEqual(normalize(np.arange(-50, 75, 25)), (np.array(-1., -0.5, 0., 0.5, 1.), True))
