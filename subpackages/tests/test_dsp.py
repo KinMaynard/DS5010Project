694700eb8c9a227c7f50dae7b30427bd5b101223
Kin Maynard <kmaynard@gm.slc.edu>
2021-10-31 18:51:13 -0400

    midside tests

--- subpackages/tests/test_dsp.py
+++ subpackages/tests/test_dsp.py
@@ -9,12 +9,14 @@ import numpy as np
 from subpackages.dsp.normalize import normalize
 from subpackages.dsp.midside import midside
 
+encoded = np.array([[ 0.5,  0.5], [ 0.5, -0.5]])
+
 class TestDsp(unittest.TestCase):
 	
 	def test_midside(self):
-		self.assertEqual(midside(array, '1'), )
-		self.assertEqual(midside(array, '2'), )
-		self.assertEqual(midside(array, '2', False), )
+		self.assertEqual(midside(np.arange(2), '1'), np.array([[0., 0.], [1., 0.]]), True)
+		self.assertEqual(midside(np.identity(2), '2'), encoded, True)
+		self.assertEqual(midside(encoded, '2', False), np.identity(2), False)
 
 	def test_normalize(self):
 		self.assertEqual(normalize(np.arange(-50, 75, 25)), (np.array(-1., -0.5, 0., 0.5, 1.), True))
