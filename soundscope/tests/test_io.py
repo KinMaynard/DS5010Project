6a6d614f6392813805622b8540ecb60993e79946
Kin Maynard <kmaynard@gm.slc.edu>
2021-11-20 12:44:18 -0500

    temp file io test  sketch

--- soundscope/tests/test_io.py
+++ soundscope/tests/test_io.py
@@ -5,11 +5,14 @@ Unit Tests
 '''
 
 import unittest
+import tempfile
 import numpy as np
 from subpackages.io.import_array import import_array
 from subpackages.io.export_array import export_array
 
 class TestIO(unittest.TestCase):
+	def setup(self):
+		# tempfile.NamedTemporaryFile(suffix='.aiff').name
 
 	def test_import_array(self):
 
