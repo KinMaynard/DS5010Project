57c81a0e9c34249dd1b519a2c096c801a84654f2
Kin Maynard <kmaynard@gm.slc.edu>
2022-01-29 20:28:31 -0500

    drafted i/o tests

--- soundscope/tests/test_io.py
+++ soundscope/tests/test_io.py
@@ -5,15 +5,24 @@ Unit Tests
 '''
 
 import unittest
-import tempfile
+import soundfile as sf
 import numpy as np
-from soundfile.io.import_array import import_array
-from soundfile.io.export_array import export_array
+import os
+from soundscope.io.import_array import import_array
+from soundscope.io.export_array import export_array
 
 class TestIO(unittest.TestCase):
 	def setup(self):
-		# tempfile.NamedTemporaryFile(suffix='.aiff').name
+		sf.write('tmp.wav', np.arange(-1, 1, .5).reshape(2, 2), 44100, 'PCM_24')
+		metadata = ('tmp.wav', '2', array([[-1. , -0.5], [ 0. ,  0.5]]), '[PCM_24]', 44100)
 
 	def test_import_array(self):
+		self.assertEqual(import_array('tmp.wav'), metadata)
 
 	def test_export_array(self):
+		export_array('tmp.wav', array([[-1. , -0.5], [ 0. ,  0.5]]), 48000, '[PCM_24]')
+		name, channels, data, subtype, sample_rate = import_array('tmp.wav')
+		self.assertEqual(sample_rate, 48000)
+
+	def tearDown(self):
+		os.remove('tmp.wav')
