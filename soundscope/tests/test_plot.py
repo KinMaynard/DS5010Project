c0fb08b45bc8faafaf36918d72925fa0f9e9c2c7
Kin Maynard <kmaynard@gm.slc.edu>
2022-01-30 20:55:14 -0500

    boilerplate unit tests for plotting functions

--- soundscope/tests/test_plot.py
+++ soundscope/tests/test_plot.py
@@ -5,6 +5,8 @@ Unit Tests
 '''
 
 import unittest
+import os
+import soundfile as sf
 import numpy as np
 from soundscope.vis.waveform import waveform
 from soundscope.vis.magnitude import magnitude
@@ -18,3 +20,22 @@ mpl.use('Qt5Agg')
 # ignore divide by 0 error in log
 np.seterr(divide = 'ignore')
 
+class TestPlot(unittest.TestCase):
+
+	def setup(self):
+		sf.write('tmp.wav', np.arange(-1, 1, .5).reshape(2, 2), 44100, 'PCM_24')
+	
+	def test_bins(self):
+
+	def test_magnitude(self):
+
+	def test_spectrogram(self):
+
+	def test_vectorscope(self):
+
+	def test_waveform(self):
+
+	def test_vectorscope(self):
+
+	def tearDown(self):
+		os.remove('tmp.wav')
