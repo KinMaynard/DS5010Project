6cd4afc8d86cf803014c3ca53d5964a29add4e7f
Kin Maynard <kmaynard@gm.slc.edu>
2022-02-04 17:25:42 -0500

    fixed 2nd bin test error

--- soundscope/tests/test_bins.py
+++ soundscope/tests/test_bins.py
@@ -14,8 +14,10 @@ class TestBins(unittest.TestCase):
 		# len divisible by bin
 		downsampled, sample_rate = bins(np.arange(128).reshape(-1, 2), '2', 128)
 		self.assertTrue((downsampled == np.array([7.5, 23.5, 39.5, 55.5, 71.5, 87.5, 103.5, 119.5])).all())
-		self.assertEqual(sample_rate, 8)
+		self.assertEqual(sample_rate, 8.0)
 		# len indivisible by bin, 1d
-		self.assertEqual(bins(np.arange(120), '1', 128), np.array([7.5, 23.5, 39.5, 55.5, 71.5, 87.5, 103.5, 115.75]), 8.0)
+		down, rate = bins(np.arange(120), '1', 128)
+		self.assertEqual((down, np.array([7.5, 23.5, 39.5, 55.5, 71.5, 87.5, 103.5, 115.75])).all())
+		self.assertEqual(rate, 8.0)
 		# len indivisible by bin, 2d
 		self.assertEqual(bins(np.arange(120).reshape(-1, 2), '2', 128), np.array([[15., 16.], [47., 48.], [79., 80.], [107., 108.]]), 8.0)
