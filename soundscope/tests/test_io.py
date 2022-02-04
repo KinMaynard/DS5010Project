6a903efe1135d7e40a2028b784954e0cc91e1046
Kin Maynard <kmaynard@gm.slc.edu>
2022-02-04 18:05:45 -0500

    fixed io tests

--- soundscope/tests/test_io.py
+++ soundscope/tests/test_io.py
@@ -12,12 +12,16 @@ from soundscope.io.import_array import import_array
 from soundscope.io.export_array import export_array
 
 class TestIO(unittest.TestCase):
-	def setup(self):
-		sf.write('tmp.wav', np.arange(-1, 1, .5).reshape(2, 2), 44100, 'PCM_24')
-		metadata = ('tmp.wav', '2', array([[-1. , -0.5], [ 0. ,  0.5]]), '[PCM_24]', 44100)
-
+	# def setup(self):
 	def test_import_array(self):
-		self.assertEqual(import_array('tmp.wav'), metadata)
+		sf.write('tmp.wav', np.arange(-1, 1, .5).reshape(2, 2), 44100, 'PCM_24')
+		metadata = ('tmp.wav', '2', np.array([[-1. , -0.5], [ 0. ,  0.5]]), '[PCM_24]', 44100)
+		name, channels, data, subtype, sample_rate = import_array('tmp.wav')
+		self.assertTrue((data == metadata[2]).all())
+		self.assertEqual(name, metadata[0])
+		self.assertEqual(channels, metadata[1])
+		self.assertEqual(subtype, metadata[3])
+		self.assertEqual(sample_rate, metadata[4])
 
 	def test_export_array(self):
 		export_array('tmp.wav', np.array([[-1. , -0.5], [ 0. ,  0.5]]), 48000, 'PCM_24')
