bdb07dbd1c5131a021caebcaed926ce2e27e8296
Kin Maynard <kmaynard@gm.slc.edu>
2022-02-04 17:54:24 -0500

    fixed midside tests

--- soundscope/tests/test_dsp.py
+++ soundscope/tests/test_dsp.py
@@ -14,9 +14,15 @@ encoded = np.array([[ 0.5,  0.5], [ 0.5, -0.5]])
 class TestDsp(unittest.TestCase):
 	
 	def test_midside(self):
-		self.assertEqual(midside(np.arange(2), '1'), np.array([[0., 0.], [1., 0.]]), True)
-		self.assertEqual(midside(np.identity(2), '2'), encoded, True)
-		self.assertEqual(midside(encoded, '2', False), np.identity(2), False)
+		coded, ms = midside(np.arange(2), '1')
+		self.assertTrue((coded == np.array([[0., 0.], [1., 0.]])).all())
+		self.assertEqual(ms, True)
+		coded2, ms2 = midside(np.identity(2), '2')
+		self.assertTrue((coded2 == encoded).all())
+		self.assertEqual(ms2, True)
+		coded3, ms3 = midside(encoded, '2', False)
+		self.assertTrue((coded3 == np.identity(2)).all())
+		self.assertEqual(ms3, False)
 
 	def test_normalize(self):
 		array, normal = normalize(np.arange(-50, 75, 25))
