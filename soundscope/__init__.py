d8541b075b36e701cc3da842f7df9b5ce5fd9cd9
Kin Maynard <morgan@Kadomony.local>
2022-02-07 17:26:50 -0500

    drafted part of package docstring

--- soundscope/__init__.py
+++ soundscope/__init__.py
@@ -1,1 +1,13 @@
-"""This is a library for the processing & visualization of audio data."""
+"""
+Audio imaging & editor Python package
+=====================================
+
+soundscope is a Python package for the imaging & editing of audio files.
+Dependancies include numpy, matplotlib, soundfile & inquirer. soundfile
+handles the import and export of audio files to and from numpy arrays.
+numpy handles the editing of the arrays of audio data. inquirer provides
+a ui for the tests requiring user interaction. matplotlib supplies the
+interface for the visualization functions. 
+
+Package: (init.py module): list modules, subpackages exported by package
+"""
